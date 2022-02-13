eed3eef1e3542a62305d513206039eab49640c91
tobslovesjiji <21150@my.sanctamaria.school.nz>
2022-02-14 09:57:18 +1300

    Blog_Post lesson 4 halfway

--- main_app.py
+++ main_app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
+posts = [
+
+    {
+       'author': 'tobs',
+       'title': 'Blog Post 1',
+       'content': 'First post content',
+       'date_posted': 'February 14th, 2022'
+    },
+
+    {
+       'author': 'jaks',
+       'title': 'Blog Post 2',
+       'content': 'Second post content',
+       'date_posted': 'February 14th, 2022'
+    }
+
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
