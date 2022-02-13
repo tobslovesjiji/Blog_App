2d556aab1cec628c57111b5e631c3c11b2ad3fcc
tobslovesjiji <21150@my.sanctamaria.school.nz>
2022-02-14 09:16:30 +1300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Home Page</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
