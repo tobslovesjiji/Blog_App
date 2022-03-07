c14a4b4161a5222d7965b6550f80df6a80336c39
tobslovesjiji <21150@my.sanctamaria.school.nz>
2022-03-07 14:05:05 +1300

    Lesson 17

--- main_app.py
+++ main_app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '0a03872ea9d2848cb5e66bf7f4e3cdfa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.string(20), unique=True, nullable=False)
+    username = db.Column(db.string(120), unique=True, nullable=False)
+    image_file = db.Column(db.string(20), nullable=False, default='default.jpg')
+    password = db.Column(db.string(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 posts = [
 
