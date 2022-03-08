c66274b52d664ec81fda8cd39cabb489f34373f7
tobslovesjiji <21150@my.sanctamaria.school.nz>
2022-03-09 12:55:41 +1300

    Lesson 21

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0a03872ea9d2848cb5e66bf7f4e3cdfa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from main_app import routes
