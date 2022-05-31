1da7a290844173b0b7c3d5446a00c3a570f31f68
tobslovesjiji <21150@my.sanctamaria.school.nz>
2022-05-31 13:06:07 +1200

    Email Server Change (Lesson 35)

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -12,11 +12,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'mail.glasse.co.nz'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'trevretubalado@gmail.com'
-app.config['MAIL_PASSWORD'] = 'insert password here'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USERNAME'] = 'reset@glasse.co.nz'
+app.config['MAIL_PASSWORD'] = 'SanctaDTG22!@'
 mail = Mail(app)
 
 from main_app import routes
